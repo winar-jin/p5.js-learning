3436eac1ddcaef2b8645c40ddfbfd00c8c6a548a
winar-jin <winar1994@outlook.com>
2016-11-13 00:32:16 +0800

    update

--- drag and drop a file/sketch.js
+++ drag and drop a file/sketch.js
@@ -1,31 +1,32 @@
-// var dropzone;
+var dropzone;
 var canvas;
 function setup() {
 	var canvas = createCanvas(200,200);
 	console.log(canvas);
-	background(0);
+	background(100);
 
-	// dropzone = select('#dropzone');
-	// dropzone.dragOver(highlight);
-	// dropzone.dragLeave(unhighlight);
-	// dropzone.drop(gotFile,unhighlight);
-	// canvas.dragOver(highlight);
-	// canvas.dragLeave(unhighlight);
-	// canvas.drop(gotFile,unhighlight);
-	canvas.drop(gotFile);
+	dropzone = select('#dropzone');
+	dropzone.dragOver(highlight);
+	dropzone.dragLeave(unhighlight);
+	dropzone.drop(gotFile,unhighlight);
+	canvas.dragOver(highlight);
+	canvas.dragLeave(unhighlight);
+	canvas.drop(gotFile,unhighlight);
+	//canvas.drop(gotFile);
 }
 
 function gotFile(file) {
 	createP(file.name + file.size);
 	var img = createImg(file.data);
-	img.hide();
-	image(img,0,0,width,height);
+	//img.show();
+	//img.size(100,100);
+	image(img,0,0,width/2,height/2);
 }
 
 
 
 function draw() {
-  background(244, 248, 252);
+  background(100, 248, 252);
     line(mouseX, mouseY, pmouseX, pmouseY);
     print(pmouseX + " -> " + mouseX);
   if(mouseIsPressed){
@@ -38,9 +39,9 @@ function draw() {
   // line(mouseX,mouseY.pmouseX,pmouseY);
 }
 
-// function highlight() {
-// 	dropzone.style('background-color','#ccc');
-// }
-// function unhighlight() {
-// 	dropzone.style('background-color','#fff');
-// }
+function highlight() {
+	dropzone.style('background-color','#ccc');
+}
+function unhighlight() {
+	dropzone.style('background-color','#fff');
+}
