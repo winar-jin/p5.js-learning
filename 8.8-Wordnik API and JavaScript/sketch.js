9a3d2d3e150b60b4cafadc80597ff17e0b55bda2
winar-jin <winar1994@outlook.com>
2016-11-17 21:53:12 +0800

    wordnik Api test

--- 8.8-Wordnik API and JavaScript/sketch.js
+++ 8.8-Wordnik API and JavaScript/sketch.js
@@ -1,7 +1,20 @@
+var link1 = "http://api.wordnik.com:80/v4/word.json/";
+var word = "fancy";
+var link2 = "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+var link = link1 + word + link2;
 function setup() {
-
+	noCanvas();
+	var caoch = createA("#",word);
+	caoch.mousePressed(getDef);
+	function getDef() {
+		loadJSON(link,getData);
+	}
 }
 
-function draw() {
+function getData(data) {
+	console.log();
+	createP(data[1].text);
+}
+// function draw() {
   
-}
+// }
