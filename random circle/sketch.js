4f150244be234723ae2633e90f6778382d5e158d
winar-jin <winar1994@outlook.con>
2016-11-30 11:05:13 +0800

    random circle

---
+++ random circle/sketch.js
@@ -0,0 +1,39 @@
+var circles = [];
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+    var protection = 0;
+    while (circles.length < 300) {
+        var circle = {
+            x: random(width),
+            y: random(height),
+            r: random(5,30)
+        };
+        //circles.push(circle);
+        // circles[0] = circle;
+        var overlapping = false;
+        for (var j = 0; j < circles.length; j++) {
+
+            var other = circles[j];
+            console.log(other);
+            var distance = dist(other.x, other.y, circle.x, circle.y);
+            if (distance < (other.r + circle.r)) {
+                overlapping = true;
+            }
+
+        }
+        if (!overlapping) {
+            circles.push(circle);
+        }
+        protection++;
+        if (protection > 1000) {
+            break;
+        }
+    }
+
+    for (var i = 0; i < circles.length; i++) {
+        fill(255, 0, 100, 100);
+        noStroke();
+        ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
+    }
+}
